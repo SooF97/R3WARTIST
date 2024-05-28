61694ec3aabe597a47238213d35f1340418930e1
SooF97 <soufien.mosad97@hotmail.com>
2024-05-28 20:16:23 +0100

    changing openai api key

--- pages/api/route.js
+++ pages/api/route.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
 export default async (req, res) => {
