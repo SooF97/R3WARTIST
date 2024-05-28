fcec6c94adc01ff19e8ff4bba557d914e43cf0b3
SooF97 <soufien.mosad97@hotmail.com>
2024-05-28 18:16:15 +0100

    Adding openai api key

--- pages/api/route.js
+++ pages/api/route.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: "sk-proj-0kY3YrsbX68YARR07MvQT3BlbkFJVQTP8XjBFEvTEgqebtvI", // This is also the default, can be omitted
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
 export default async (req, res) => {
