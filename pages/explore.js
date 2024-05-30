6b11a6dfe2b4d8c3dab83b821487a2fc19b04a9c
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2024-05-30 12:52:26 +0100

    explore page ui done

--- pages/explore.js
+++ pages/explore.js
@@ -41,65 +41,95 @@ const explore = () => {
 
   return (
     <>
-      <section className="container mx-auto p-4">
-        <div>
-          {fetchingData && (
-            <div className="flex justify-center">
-              <Loading type="spin" color="black" height={50} width={50} />
+      <section
+        style={{
+          paddingTop: "8%",
+        }}
+        id="nftcollections"
+        className="nftcollections"
+      >
+        <div className="container">
+          <div>
+            {fetchingData && (
+              <div className="flex justify-center">
+                <Loading type="spin" color="black" height={50} width={50} />
+              </div>
+            )}
+          </div>
+          <br />
+
+          <div className="mynftsec">
+            <div className="row icon-boxes">
+              {resultPrompts.map((prompt, index) => (
+                <div
+                  key={index}
+                  className="col-lg-3 col-md-4 d-flex align-items-stretch"
+                >
+                  <div className="card">
+                    <div className="flex space-x-2 mt-2">
+                      {prompt[3].map((image, index) => (
+                        <Link href={image} target="_blank">
+                          <img
+                            key={index}
+                            src={image}
+                            alt="prompt"
+                            className="w-20 h-20 object-cover rounded"
+                          />
+                        </Link>
+                      ))}
+                    </div>
+
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <span>Artist id:</span>
+                        {""}#{prompt[1].toString()}
+                      </h5>
+                      <h5 className="card-title">
+                        <span className="font-bold">Purchased:</span>
+                        {""}
+                        {prompt[6].toString()}
+                        {""}Times
+                      </h5>
+                    </div>
+
+                    <div className="price-container">
+                      <span className="price left">
+                        <span style={{ color: "gray" }}>Artist Address:</span>
+                        <br></br>
+                        <span style={{ color: "white" }} id="pricetag">
+                          {""}
+                          {prompt[0].slice(0, 4)}...{prompt[0].slice(-4)}
+                        </span>
+                      </span>
+                      <span className="price right">
+                        <span style={{ color: "gray" }}>Price:</span> <br></br>
+                        <span style={{ color: "white" }} id="pricetag">
+                          {prompt[4].toString()}$
+                        </span>
+                      </span>
+                    </div>
+                    <Link href={`/${prompt[1].toString()}`}>
+                      <button
+                        className="joinbut"
+                        style={{ display: "flex", alignItems: "center" }}
+                      >
+                        Buy
+                        <svg
+                          className="svgIcon"
+                          viewBox="0 0 576 512"
+                          style={{ marginRight: "8px" }}
+                        >
+                          <path d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z"></path>
+                        </svg>
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              ))}
             </div>
-          )}
+          </div>
         </div>
       </section>
-      <div className="container mx-auto p-4">
-        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ">
-          {resultPrompts.map((prompt, index) => (
-            <div key={index} className="border rounded-lg p-4 bg-gray-100">
-              <h4 className="text-md">
-                <span className="font-bold">Artist Address:</span>
-                {""}
-                {prompt[0].slice(0, 4)}...{prompt[0].slice(-4)}
-              </h4>
-              <h4 className="text-md">
-                <span className="font-bold">Artist id:</span>
-                {""}#{prompt[1].toString()}
-              </h4>
-              <h4 className="text-md">
-                <span className="font-bold">Price:</span>
-                {""}
-                {prompt[4].toString()}$
-              </h4>
-              <h4 className="text-md">
-                <span className="font-bold">Purchased:</span>
-                {""}
-                {prompt[6].toString()}
-                {""}Times
-              </h4>
-              <div className="flex space-x-2 mt-2">
-                {prompt[3].map((image, index) => (
-                  <Link href={image} target="_blank">
-                    <img
-                      key={index}
-                      src={image}
-                      alt="prompt"
-                      className="w-20 h-20 object-cover rounded"
-                    />
-                  </Link>
-                ))}
-              </div>
-              <div className="flex flex-col justify-center items-center gap-2 mt-2">
-                <Link href={`/${prompt[1].toString()}`}>
-                  <button className="Btn">
-                    Buy
-                    <svg className="svgIcon" viewBox="0 0 576 512">
-                      <path d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z"></path>
-                    </svg>
-                  </button>
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
     </>
   );
 };
